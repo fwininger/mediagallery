3e79a12331233db616037ec530e3f6344073b654
Florian Wininger <fw.centrale@gmail.com>
2014-06-07 12:19:19 +0200

    Configuration file
    Signed-off-by: Florian Wininger <fw.centrale@gmail.com>

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+// Copyright (c) 2014 The MediaGallery Authors.
+// All rights reserved.
+//
+// GNU GENERAL PUBLIC LICENSE
+// See LICENSE file.
+
+// Configuration File.
+
+$GLOBALS['CONFIG_path'] = __DIR__."/../";
+$GLOBALS['CONFIG_photo'] = $GLOBALS['CONFIG_path']."photos/";
+
+$GLOBALS['CONFIG_miniature'] = 300;
+$GLOBALS['CONFIG_diaporama'] = 1100;
+
+?>
