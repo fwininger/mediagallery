1c103b047de7f1d789b0cc5e88e0ee1b9a02fcac
Florian W <fw.centrale@gmail.com>
2014-07-13 23:08:39 +0200

    Add expand function of tumbnail.

--- app/views/gallery.view.php
+++ app/views/gallery.view.php
@@ -1,3 +1,30 @@
+<script>
+$(document).ready(function() {
+    $(".myDiv").click(function() {
+		if (!$(this).hasClass("info") ) {
+			$(this).addClass("info");
+			$(this).animate({
+				width: '100%',
+			}, 500);
+			var url = $(this).children('.myImg').attr("src").replace("thumbnail", "exif");
+			$(this).children('.myExif').load(encodeURI(url));
+			$(this).children('.myExif').show();
+
+			var src = $(this).children('.myImg').attr("src").replace("thumbnail", "thumbbig");
+			$(this).children('.myImg').attr("src", src);
+		} else {
+			$(this).removeClass("info");
+			$(this).animate({
+				width: '25%',
+			}, 500);
+			$(this).children('.myExif').hide();
+
+			var src = $(this).children('.myImg').attr("src").replace("thumbbig", "thumbnail");
+			$(this).children('.myImg').attr("src", src);
+		}
+    });
+});
+</script>
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
 	<?php
 		$root = explode("/", $this->http->get('dir'));
@@ -25,13 +52,14 @@
 			$p .= $this->http->get('dir').'/';
 			$p .= $files[$i];
 			?>
-			<div class="col-xs-6 col-sm-3 placeholder">
+			<div class="col-xs-6 col-sm-3 placeholder myDiv">
 			<?php if(Jpeg::isValid($p)) { ?>
-				<img src="thumbnail?dir=<?php echo $this->http->get('dir'); ?>&image=<?php echo $files[$i]; ?>" class="img-responsive" alt="<?php echo $files[$i] ?>">
+				<img src="thumbnail?dir=<?php echo $this->http->get('dir'); ?>&image=<?php echo $files[$i]; ?>" class="img-responsive myImg" alt="<?php echo $files[$i] ?>">
 			<?php } else { ?>
 				<img src="public/images/video-clip.png" class="img-responsive" alt="<?php echo $files[$i] ?>">
 			<?php } ?>
 				<h4><?php echo $files[$i] ?></h4>
+				<div class="myExif"></div>
 			</div>
 			<?php
 		}
--- app/views/infos.view.php
+++ app/views/infos.view.php
@@ -1,17 +1,10 @@
-<img src='image.php?<?php
-echo $this->vars['paramDirectory']."="
-	.$this->vars['valueDirectory']."&image="
-	.$this->vars['picture'];
-?>' />
-<br/>
-<br/>
 <?php
 $exif = $this->vars['exif'];
 
 if($exif != false) {
 	$resolutionMP = round($exif["ExifImageWidth"] *  $exif["ExifImageLength"] / 1000000 , 1);
 ?>
-	<b>Propriétés de l'image :</b>
+	<b>PropriÃ©tÃ©s de l'image :</b>
 	<table width="100%">
 		<tr>
 			<td> Nom : <?php echo  $exif["FileName"]; ?> </td>
@@ -20,17 +13,17 @@ if($exif != false) {
 		</tr>
 		<tr>
 			<td> Taille du fichier : <?php echo  round($exif["FileSize"] / 1048576, 2); ?> Mo </td>
-			<td> Résolution : <?php echo $resolutionMP ; ?> MPixels </td>
+			<td> RÃ©solution : <?php echo $resolutionMP ; ?> MPixels </td>
 			<td> Focale : <?php echo "F/".round(substr($exif["FNumber"],0,2) / 10,1); ?></td>
 		</tr>
 		<tr>
 			<td> Date : <?php echo $exif["DateTimeOriginal"]; ?></td>
 			<td> Appareil : <?php echo $exif["Make"]." ".$exif["Model"]; ?></td>
-			<td> Temps d'exposition : <?php $temps = substr($exif["ExposureTime"],3) / 10;  echo "1/".$temps."ème"; ?> </td>
+			<td> Temps d'exposition : <?php $temps = substr($exif["ExposureTime"],3) / 10;  echo "1/".$temps."Ã¨me"; ?> </td>
 		</tr>
 	</table>
 <?php
 }
 ?>
 </br/>
-Télécharger l'image originale : <a href=''>télécharger</a>
+TÃ©lÃ©charger l'image originale : <a href=''>tÃ©lÃ©charger</a>
