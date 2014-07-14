4ad572d1a8af9ace1e41403a89d81039e1fbc345
Florian W <fw.centrale@gmail.com>
2014-07-14 22:10:55 +0200

    Update navbar.

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -11,10 +11,9 @@
 		</div>
 		<div class="navbar-collapse collapse">
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#">Dashboard</a></li>
-				<li><a href="#">Settings</a></li>
-				<li><a href="#">Profile</a></li>
-				<li><a href="#">Help</a></li>
+				<li><a href="slider?dir=<?php echo $this->http->get('dir'); ?>">Diaporama</a></li>
+				<li><a href="#">Paramètres</a></li>
+				<li><a href="#">Partage</a></li>
 			</ul>
 		</div>
 	</div>
